5e844e7b3463b232b97d35f7a43beb73ec6bef58
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-11 07:48:56 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+//ESERCIZIO:
+//DARE IN INGRESSO STRINGHE NUMERICHE
+//E STAMPARE IN ORDINE CRESCENTE E 
+//DECRESCENTE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int i;
+
+int main(int argc, char *argv[])
+{
+  int n=0, somma=0;
+        for(i=0;i<argc;i++)
+          { 
